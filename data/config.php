526af569568011d583d7c94d17e59e3c28c52c68
t.iitsuka <takumi99701@gmail.com>
2022-12-20 23:33:22 -0800

    master:addToDB folder added

--- data/config.php
+++ data/config.php
@@ -1,4 +1,8 @@
 <?php
+    $hostName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbName = "final_db";
     $baseName = "http://localhost/PHP_final_project/";
     session_start();
 ?>
